c1d1260a90692dc2ba867894de6ae57ea8018c2d
grant-sobkowski <grant.sobkowski@gmail.com>
2026-02-24 08:38:39 -0500

    add list-topics

---
+++ cmd/list_topics.go
@@ -0,0 +1,48 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"sort"
+	"time"
+
+	"github.com/grant-sobkowski/frogo-cli/internal/config"
+	"github.com/spf13/cobra"
+	"github.com/twmb/franz-go/pkg/kadm"
+)
+
+var listTopicsCmd = &cobra.Command{
+	Use:   "list-topics",
+	Short: "List all topics in the cluster",
+	RunE:  runListTopics,
+}
+
+func init() {
+	rootCmd.AddCommand(listTopicsCmd)
+}
+
+func runListTopics(cmd *cobra.Command, args []string) error {
+	cl, err := config.Client(profile)
+	if err != nil {
+		return err
+	}
+	defer cl.Close()
+
+	adminClient := kadm.NewClient(cl)
+	defer adminClient.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	topics, err := adminClient.ListTopics(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to list topics: %w", err)
+	}
+
+	names := topics.Names()
+	sort.Strings(names)
+	for _, name := range names {
+		fmt.Println(name)
+	}
+	return nil
+}
